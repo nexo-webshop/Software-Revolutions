3d57cc9e2f94166c64ef8f1b637ed2db9f2767e2
linux-rewritten-kernels <feddewestrahotmail@icloud.com>
2026-03-27 18:46:20 +0100

    Create workspace.py

---
+++ Multi Language developemt Platform/Core Engine/project management/workspace.py
@@ -0,0 +1,11 @@
+class Workspace:
+    def __init__(self, root):
+        self.root = root
+        self.files = []
+
+    def scan(self):
+        # scan directory tree
+        pass
+
+    def get_files_by_language(self, lang):
+        pass
